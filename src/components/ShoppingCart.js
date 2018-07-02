c282abfafd7a3e2dd44fab588daeaaa4ac06512d
Pablo Coronel <pablomanuelcoronel@gmail.com>
2018-07-02 20:12:36 -0300

    hecho video 2

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -52,7 +52,10 @@ class ShoppingCart extends Component {
   }
 
   removeFromCart(product) {
-
+    store.dispatch({
+      type : "REMOVE_FROM_CART",
+      product
+    })
   }
 }
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import {createStore} from 'redux';
 
 const reducer = (state, action) => {
-
 	if (action.type === "ADD_TO_CART") {
 		return {
 			...state,
 			cart : state.cart.concat(action.product)
+		}
+	}else if (action.type === "REMOVE_FROM_CART") {
+		return {
+			...state,
+			cart : state.cart.filter(prod => prod.id !== action.product.id)
 		}
 	}
 
